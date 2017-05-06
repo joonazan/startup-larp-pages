42eab3fae19218b4d01bd40ee3d996c48122dbc8
Joonatan Saarhelo <joon.saar@gmail.com>
2017-05-06 12:20:56 +0300

    tekstiä, mitä sivuun voisi tulla

--- src/Frontpage.elm
+++ src/Frontpage.elm
@@ -5,6 +5,16 @@ import Classes exposing (class, Classes(..))
 
 view model =
     div [class [Flip]]
-        [ h1 [class [Front]] [ text "Otsikko" ]
+        [ div [class [Front]]
+            [ h1 [] [ text "Otsikko" ]
+            , p [] [ text "Korppi tuottaa rikastettuja palveluita tuodakseen käyttäjän lähemmäs tuotetta." ]
+            ]
         , h1 [class [Back]] [ text "Otsikko2"]
         ]
+
+{-
+    MacroHard tekee genrenmäärittäviä seuraavan sukupolven First-person MMO RTS -ratkaisuja.
+    Kuva metsästä: "Not actual in-game footage."
+    Taustakuvina stock foto, kuva pörssistä, kuva kepistä, kuva kanaliemestä
+    kaavioita, missä näkyy esim. myyntiluvut (palkit pitempiä Macrohardilla, mutta numerot pienempiä)
+-}
